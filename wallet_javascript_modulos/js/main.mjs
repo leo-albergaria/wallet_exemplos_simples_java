53bd647cab998cad3ac8cbc861817feefc1003fe
Léo Albergaria <63436406+leo-albergaria@users.noreply.github.com>
2021-10-28 08:37:57 -0300

    Delete wallet_javascript_modulos directory

--- wallet_javascript_modulos/js/main.mjs
+++
@@ -1,5 +0,0 @@
-import { mostraCidade, mostraHobby, mostraIdade } from "/js/funcoes.js";
-
-console.log(mostraIdade("Stephany", 415));
-console.log(mostraCidade("Stephany", "São Paulo"));
-console.log(mostraHobby("Stephany", "Nadar"));
